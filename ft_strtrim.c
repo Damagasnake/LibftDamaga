f20dc9722f85c6e03bb16011e37f484e09faec35
David Martinez Gallego <davidma2@c2r12s4.42madrid.com>
2024-10-01 18:03:23 +0200

    Splitttt

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: davidma2 <davidma2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 14:51:28 by davidma2          #+#    #+#             */
-/*   Updated: 2024/10/01 10:58:42 by davidma2         ###   ########.fr       */
+/*   Updated: 2024/10/01 14:29:37 by davidma2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,9 @@ char    *ft_strtrim(char const *s1, char const *set)
 	while (end > start && ft_strchr(set, s1[end]))
 			end--;
 			len = end - start + 1;
-	trimmed_str = (char *)malloc((len + 1) * sizeof(char));
 	if (!trimmed_str)
 		return (NULL);
 	trimmed_str = ft_substr(s1, &s1[start], len + 1);
 	return (trimmed_str);
 	}
-							
+	
---
+++ splitp.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   splitp.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davidma2 <davidma2@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 17:41:15 by davidma2          #+#    #+#             */
+/*   Updated: 2024/10/01 17:41:41 by davidma2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+char **ft_split(char const *s, char c)
+{
+	int nstr; // Number of new substrs. to generate
+	int i; // scan *s.
+	int start; // Locate the start of each substr.
+	char *substr; //Work with each substr. b4 res.
+	char **res; // Storage substr.
+	int len; // Lenght of each substr.
+	int index; // Pos. in res.
+	if (s == NULL)
+	{
+		return(NULL);
+	}
+	while(s[i] && s[i] + 1 != c && s[i] != c)
+	{
+		i++;
+	}
+	nstr = 0;
+	i = 0;
+	while(s[i])
+	{
+		if (s[i] == c)
+		{
+			i++;
+		}
+			else
+			{
+			nstr++;
+			while(s[i] && s[i] != c)
+			{
+				i++;
+			}
+		}
+	}
+	res = (char **)malloc(sizeof(char *) * (nstr + 1));
+	if (res == NULL)
+	{
+		return(NULL);
+	}
+	i = 0;
+	index = 0;
+	while(s[i])
+	{
+		if (s[i] == c)
+		{
+			i++;
+		}
+		else
+		{
+			start = i;
+			while(s[i] && s[i] != c)
+			{
+				i++;
+			}
+			len = i - start;
+			substr = (char *)malloc(sizeof(char) * (len + 1));
+			if (substr == NULL)
+			{
+			    return(NULL);
+			}
+			ft_strlcpy(substr, s + start, len + 1);
+			res[index] = substr;
+			index++;
+		}
+	}
+	res[index] = NULL;
+	return(res);
+}
