cc34e8e1874aef4a0071b8dbe7ae384c02ec15ed
David Martinez Gallego <davidma2@c2r12s4.42madrid.com>
2024-10-09 11:40:42 +0200

    divide split

--- ft_split.c
+++ ft_split.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: davidmartinezgallego <davidmartinezgall    +#+  +:+       +#+        */
+/*   By: davidma2 <davidma2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 17:41:15 by davidma2          #+#    #+#             */
-/*   Updated: 2024/10/07 14:37:06 by davidmartin      ###   ########.fr       */
+/*   Updated: 2024/10/09 11:38:26 by davidma2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 char	**ft_split(char const *s, char c)
 {
@@ -27,49 +26,37 @@ char	**ft_split(char const *s, char c)
 	nstr = 0;
 	i = 0;
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 	while (s[i] && s[i + 1] != c && s[i] != c)
-	{
 		i++;
-	}
 	i = 0;
 	while (s[i])
 	{
 		if (s[i] == c)
-		{
 			i++;
-		}
 		else
 		{
 			nstr++;
 			while (s[i] && s[i] != c)
-			{
 				i++;
-			}
 		}
 	}
-	res = (char **)malloc(sizeof(char *) * (nstr + 1));
+
+	res = allocatemem(nstr);
 	if (res == NULL)
-	{
 		return (NULL);
-	}
+
 	i = 0;
 	index = 0;
 	while (s[i])
 	{
 		if (s[i] == c)
-		{
 			i++;
-		}
 		else
 		{
 			start = i;
 			while (s[i] && s[i] != c)
-			{
 				i++;
-			}
 			len = i - start;
 			substr = (char *)malloc(sizeof(char) * (len + 1));
 			if (substr == NULL)
@@ -87,5 +74,15 @@ char	**ft_split(char const *s, char c)
 		}
 	}
 	res[index] = NULL;
+	return (res);
+}
+
+static char **allocatemem(int nstr)
+{
+	char **res;
+
+	res = (char **)malloc(sizeof(char *) * (nstr + 1));
+	if (res == NULL)
+		return (NULL);
 	return (res);
 }
