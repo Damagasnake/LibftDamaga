bfb31e7e15f4f5bcfc72dee184c38099c06bb9cf
David Martinez Gallego <davidma2@c2r12s4.42madrid.com>
2024-10-11 14:02:01 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: davidma2 <davidma2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 17:41:15 by davidma2          #+#    #+#             */
-/*   Updated: 2024/10/11 10:54:31 by davidma2         ###   ########.fr       */
+/*   Updated: 2024/10/11 14:00:21 by davidma2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,100 +14,58 @@
 #include <stdlib.h>
 #include <string.h>
 
-static char	**allocatemem(int nstr)
+static void	free_memory(char **res, int index)
 {
-	char	**res;
-
-	res = (char **)malloc(sizeof(char *) * (nstr + 1));
-	if (res == NULL)
-		return (NULL);
-	return (res);
-}
-
-static void	countstr(const char *s, int *nstr, char c)
-{
-	int	i;
-
-	i = 0;
-	if (s == NULL)
-		return ;
-	while (s[i] && (s[i + 1] != c || s[i] != c))
-		i++;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			i++;
-		else
-		{
-			(*nstr)++;
-			while (s[i] && s[i] != c)
-				i++;
-		}
-	}
-}
-
-static void	checknull(char *substr, int index, char **res)
-{
-	if (substr == NULL)
-	{
-		while (index > 0)
-			free(res[--index]);
-		free(res);
-		return ;
-	}
+	while (index > 0)
+		free(res[--index]);
+	free(res);
 }
 
 static void	divide_s(const char *s, char c, char **res)
 {
 	int		i;
-	int		start;
-	int		index;
-	int		len;
 	char	*substr;
 
-	i = 0;
-	index = 0;
+	i = 0, start;
+	i = 0, start = 0, index;
+	i = 0, start = 0, index = 0, len;
 	while (s[i])
 	{
 		if (s[i] == c)
-			i++;
-		else
 		{
-			start = i;
-			while (s[i] && s[i] != c)
-				i++;
-			len = i - start;
-			substr = (char *)malloc(sizeof(char) * (len + 1));
-			checknull(substr, index, res);
-			strncpy(substr, s + start, len);
-			substr[len] = '\0';
-			res[index++] = substr;
+			i++;
+			continue ;
 		}
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if ((len = i - start) == 0)
+			continue ;
+		substr = (char *)malloc(sizeof(char) * (len + 1));
+		if (substr == NULL)
+		{
+			free_memory(res, index);
+			return ;
+		}
+		strncpy(substr, s + start, len);
+		substr[len] = '\0';
+		res[index++] = substr;
 	}
 }
+
 char	**ft_split(const char *s, char c)
 {
 	int		nstr;
 	char	**res;
-	int		index;
 
-	index = 0;
 	nstr = 0;
+	if (s == NULL)
+		return (NULL);
 	countstr(s, &nstr, c);
 	res = allocatemem(nstr);
 	if (res == NULL)
 		return (NULL);
 	divide_s(s, c, res);
-	res[index] = NULL;
+	res[nstr] = NULL;
 	return (res);
 }
-/*
-#include <stdio.h>
-
-int	main(void)
-{
-	char	string[] = "viva la vida";
-
-	return (0);
-}*/
