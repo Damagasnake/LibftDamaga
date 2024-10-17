98d055eb984f382dbfe4d410e29da5285792ed83
David Martinez Gallego <davidma2@c2r12s4.42madrid.com>
2024-10-17 10:15:10 +0200

    Libft Final

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: davidma2 <davidma2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 18:56:04 by davidmartin       #+#    #+#             */
-/*   Updated: 2024/10/14 14:50:59 by davidma2         ###   ########.fr       */
+/*   Updated: 2024/10/17 10:14:42 by davidma2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	long	b;
-
-	b = n;
-	if (b < 0)
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		b = b * -1;
+		n = n * -1;
 	}
-	if (b > 9)
+	if (n > 9)
 	{
-		ft_putnbr_fd(b / 10, fd);
-		ft_putnbr_fd(b % 10, fd);
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
 	else
-		ft_putchar_fd(b + '0', fd);
+		ft_putchar_fd(n + '0', fd);
 }
 
 // void ft_putnbr_fd(int n, int fd)
