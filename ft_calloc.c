d379e76f64baae56f27b1e1766390b471c7ffae0
Damaga <damagasnake@gmail.com>
2025-01-16 15:09:54 +0100

    Libft changes

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,12 +6,11 @@
 /*   By: davidma2 <davidma2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 10:27:11 by davidma2          #+#    #+#             */
-/*   Updated: 2024/10/17 10:25:42 by davidma2         ###   ########.fr       */
+/*   Updated: 2025/01/16 15:08:58 by davidma2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
